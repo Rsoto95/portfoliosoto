5d646c37e2674d7e57f2438a02a957c7920d0cb9
Rsoto95 <roberto-soto.com@hotmail.com>
2022-12-25 15:40:52 -0600

    adding contact me features

--- src/components/contactMe/contactMe.js
+++ src/components/contactMe/contactMe.js
@@ -1,13 +1,13 @@
 import "./contactMe.css";
 import flecha from "./flecha.png";
-import emailjs from '@emailjs/browser';
-
-import React, { useRef } from "react";
-console.log(emailjs);
+import emailjs from "@emailjs/browser";
+import React, { useEffect, useRef, useState } from "react";
 
 const Contact = () => {
   const form = useRef();
 
+  const [currentHeight, setHeight] = useState("5rem");
+  const [messageSent,setMessageSent]=useState('SEND')
 
   function sendEmail(e) {
     e.preventDefault();
@@ -16,40 +16,55 @@ const Contact = () => {
       .sendForm(
         "service_otnnsz4",
         "template_kse57bh",
-        e.target,
+        form.current,
         "yfVnptrpAtg26ijh-"
       )
       .then(
-        (result ) => {
+        (result) => {
           console.log(result.text);
+          setMessageSent("Message sent, will reply asap!")
         },
         (error) => {
           console.log(error.text);
+          setMessageSent("There was an error please try again")
         }
       );
   }
+
+
+  let checkHeight = () => {
+    let textArea = document.getElementsByClassName("text-area");
+
+    let textAreaHeight = textArea[0].scrollHeight;
+
+    if (textAreaHeight > 50) {
+      setHeight(`${textAreaHeight / 10}rem`);
+    }
+  };
 
   return (
     <section className="wholeContact">
       <div className="contact">
         <h1 className="contactWord">CONNECT WITH ME</h1>
-        <form>
+        <form className="contact" ref={form} onSubmit={sendEmail}>
           <input
-            type="text"
+            type="email"
             id="EMAIL"
-            name="EMAIL"
+            name="user_email"
             placeholder="EMAIL"
           ></input>
           <textarea
             className="text-area"
+            name="message"
             rows={20}
             cols={20}
             placeholder="ENTER YOUR MESSAGE"
+            onChange={() => {
+              checkHeight();
+            }}
+            style={{ height: `${currentHeight}` }}
           ></textarea>
-          <button className="flecha">
-            <span>SEND MESSAGE</span>
-            <img src={flecha} alt="Flecha" />
-          </button>
+          <input type="submit" value={messageSent} className="flecha"></input>
         </form>
       </div>
     </section>
